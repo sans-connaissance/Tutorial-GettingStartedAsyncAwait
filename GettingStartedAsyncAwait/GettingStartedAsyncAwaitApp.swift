bebe55c4c168258a531753d9474f48bbcf8f283a
Mohammad Azam <azamsharp@gmail.com>
2021-07-09 12:09:23 -0500

    Initial Commit

---
+++ GettingStartedAsyncAwait/GettingStartedAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedAsyncAwaitApp.swift
+//  GettingStartedAsyncAwait
+//
+//  Created by Mohammad Azam on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
